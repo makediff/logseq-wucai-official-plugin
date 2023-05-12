ce73db771e5bcd8615529656c0706888d8ab32c9
飞刀 <makediff@qq.com>
2023-05-12 17:50:14 +0800

    change version

--- src/bgconsts.ts
+++ src/bgconsts.ts
@@ -3,8 +3,8 @@ export class BGCONSTS {
   static SERVICE_ID: number = 11
   static MARKET_INFO: string = 'logseq-plugin'
   static ENDPOINT: string = 'logseqwucai'
-  static VERSION: string = '23.5.9'
-  static VERSION_NUM: number = 230509
+  static VERSION: string = '23.5.12'
+  static VERSION_NUM: number = 230512
   static IS_DEBUG: boolean = true
   static TEST_TOKEN: string = ''
   static BASE_URL: string = 'https://marker.dotalk.cn'
